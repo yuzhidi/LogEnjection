96e97ca9fa8c7a08156c342240c7e2e0d79c171d
Leo Cone <wlia@163.com>
2015-07-06 11:41:38 +0800

    first version

--- logEject.sh
+++ logEject.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+#############################
+# Leo Tools
+#############################
+
 DEBUG= # open echo
 FILE_NAME=
 LINE_NUM=
 COND=
 FOUND_COUNT=
-TMP_BASE_NAME=
+TMP_DIR_BASE_NAME=
 BASE_FILE_NAME=
 LINE_ADDR=
 FOUND_BRACE=
@@ -20,7 +24,7 @@ function funcFindBrace() {
     # asume max 4 line.
     local i=0
     BraceExitLine=${LINE_ADDR}
-    grep -n "$COND" ${TMP_BASE_NAME} | tee /tmp/tmpGrepCond
+    grep -n "$COND" ${TMP_DIR_BASE_NAME} | tee /tmp/tmpGrepCond
     #echo $tmpStr
     for((;i<=3;i++))
     do
@@ -32,8 +36,7 @@ function funcFindBrace() {
                 i=4 #return
             else
                 BraceExitLine=$((${BraceExitLine}+1))
-		# sed -n
-                sed -n "${BraceExitLine}, ${BraceExitLine} p" ${TMP_BASE_NAME} >> /tmp/tmpGrepCond
+                sed -n "${BraceExitLine}, ${BraceExitLine} p" ${TMP_DIR_BASE_NAME} >> /tmp/tmpGrepCond
                 cat /tmp/tmpGrepCond
         fi
     done
@@ -48,27 +51,23 @@ function funcFindBrace() {
     fi
     #################################################
     # check super
-    sed -n "$((${BraceExitLine}+1)), $((${BraceExitLine}+1)) p" ${TMP_BASE_NAME} > /tmp/tmpSuper
-    local FOUND_SUPER=`grep "super" /tmp/tmpSuper`
-    if test ${FOUND_SUPER}
+    sed -n "$((${BraceExitLine}+1)), $((${BraceExitLine}+1)) p" ${TMP_DIR_BASE_NAME} > /tmp/tmpSuper
+    local FOUND_SUPER=`grep "super" /tmp/tmpSuper|wc -l`
+    if test ${FOUND_SUPER} -ne 0
         then
                 BraceExitLine=$((${BraceExitLine}+1))
     fi
     ##################################################
     # eject
-    echo "eject"
+    #echo "eject"
 
-    # sed 's/ x / x /g'
     sed 's/\s/ /g' /tmp/tmpGrepCond |  sed 's/^[[:space:]]*//g' > /tmp/tmpGrepCond_tmp
 
     echo "**************************************************"
-    # cat | tr
-    LOG_SRC=` cat /tmp/tmpGrepCond_tmp | tr "\n" " " ; echo `
+    LOG_SRC=` cat /tmp/tmpGrepCond_tmp | tr '\n{' " " ; echo`
     echo ${LOG_SRC}
-
-    # sed -e script, --expression=script
-    sed -e "${BraceExitLine} a\ Log.d(TAG,\"leoAutoLog_${LOG_SRC} ENTER\");" ${TMP_BASE_NAME} > /tmp/source_temp.java
-    mv /tmp/source_temp.java ${TMP_BASE_NAME}
+    sed -e "${BraceExitLine} a\ Log.d(TAG,\"leoAutoLog ${LOG_SRC}\");" ${TMP_DIR_BASE_NAME} > /tmp/source_temp.java
+    mv /tmp/source_temp.java ${TMP_DIR_BASE_NAME}
     #sleep 10 #debug
     echo "#################################################"
 }
@@ -77,12 +76,17 @@ function funcEjectFunctionEnterLog() {
     LINE_ADDR=
     FOUND_BRACE=
 
-    # awk -F:
-    LINE_ADDR=`grep -n "$COND" ${TMP_BASE_NAME} | awk -F: '{print $1}'`
+    LINE_ADDR=`grep -n "$COND" ${TMP_DIR_BASE_NAME} | awk -F: '{print $1}'`
     echo ${LINE_ADDR}
     #call funcFindBrace
     funcFindBrace
 }
+
+##############################################################################
+#
+#  main
+#
+###########################################################
 
 #echo $*
 echo $1
@@ -107,46 +111,59 @@ fi
 
 BASE_FILE_NAME=`basename ${FILE_NAME}`
 echo "base name:${BASE_FILE_NAME}"
-TMP_BASE_NAME="/tmp/${BASE_FILE_NAME}"
+TMP_DIR_BASE_NAME="/tmp/${BASE_FILE_NAME}"
 
+############################
 # create tmp source file
 cp ${FILE_NAME} ${TMP_DIR_BASE_NAME}
 
 echo "" > ${TMP_DIR_BASE_NAME}_unmatch
-
-# ctags --language-force=java -f
+#########################
+# ctags
 echo "ctags --language-force=java -f '${TMP_DIR_BASE_NAME}_tags' ${TMP_DIR_BASE_NAME}"
-ctags --language-force=java -f "/tmp/${TMP_DIR_BASE_NAME}_tags" ${TMP_DIR_BASE_NAME}
+ctags --language-force=java -f "${TMP_DIR_BASE_NAME}_tags" ${TMP_DIR_BASE_NAME}
 
-# grep '	m	'
-echo "grep '	m	' /tmp/${TMP_DIR_BASE_NAME}_tags > /tmp/TMP_BASE_NAME_method_tags"
-grep '	m	' /tmp/${TMP_DIR_BASE_NAME}_tags > TMP_BASE_NAME_method_tags
+#######################
+# only methods
+echo "grep '	m	' ${TMP_DIR_BASE_NAME}_tags > TMP_DIR_BASE_NAME_method_tags"
+grep '	m	' ${TMP_DIR_BASE_NAME}_tags > TMP_DIR_BASE_NAME_method_tags
 
-# grep -o , --only-matching
-# awk -F ' XX ' '{print $X}'
-echo 'grep -o "/\\^.*\\$" TMP_BASE_NAME_method_tags | awk -F '/\\^' '{print $2}' | awk -F '\\$' '{print $1}' >  /tmp/TMP_BASE_NAME_method_def_line_tags'
-grep -o "/\\^.*\\$" TMP_BASE_NAME_method_tags | awk -F '/\\^' '{print $2}' | awk -F '\\$' '{print $1}' >  /tmp/TMP_BASE_NAME_method_def_line_tags
+# print cmd
+echo 'grep -o "/\\^.*\\$" TMP_DIR_BASE_NAME_method_tags | awk -F '/\\^' '{print $2}' | awk -F '\\$' '{print $1}' >  ${TMP_DIR_BASE_NAME}_method_def_line_tags'
+grep -o "/\\^.*\\$" TMP_DIR_BASE_NAME_method_tags | awk -F '/\\^' '{print $2}' | awk -F '\\$' '{print $1}' >  ${TMP_DIR_BASE_NAME}_method_def_line_tags
 
-# wc
-wc /tmp/TMP_BASE_NAME_method_def_line_tags
-LINE_NUM=`wc -l /tmp/TMP_BASE_NAME_method_def_line_tags | awk '{print $1}'`
+wc ${TMP_DIR_BASE_NAME}_method_def_line_tags
+LINE_NUM=`wc -l ${TMP_DIR_BASE_NAME}_method_def_line_tags | awk '{print $1}'`
 echo ${LINE_NUM}
 
-# sed -n "${i}, ${i} p"
+
 for((i=1;i<=${LINE_NUM};i++))
 do
-    #grep "`sed -n "${i}, ${i} p" /tmp/TMP_BASE_NAME_method_def_line_tags`" ${TMP_BASE_NAME} | wc -l >> /tmp/ttt1
-    COND=`sed -n "${i}, ${i} p" /tmp/TMP_BASE_NAME_method_def_line_tags`
-    FOUND_COUNT=`grep "$COND" ${TMP_BASE_NAME} | wc -l`
+    #
+    # only handle only match once
+    #
+    COND=`sed -n "${i}, ${i} p" ${TMP_DIR_BASE_NAME}_method_def_line_tags`
+    FOUND_COUNT=`grep "$COND" ${TMP_DIR_BASE_NAME} | wc -l`
+
+    # grep fail
     if test $? -eq 1
         then
             echo ${COND}
+            echo "fail ${COND}" >> ${TMP_DIR_BASE_NAME}_unmatch
+    # not match once
     elif test ${FOUND_COUNT} -ne 1
         then
             echo ${FOUND_COUNT} "   " ${COND}
+            echo "${FOUND_COUNT} ${COND}" >> ${TMP_DIR_BASE_NAME}_unmatch
     else
         # call function
         funcEjectFunctionEnterLog
     fi
 done
 
+echo "*************** unmatch number*********************"
+cat ${TMP_DIR_BASE_NAME}_unmatch
+echo ""
+echo "**************unmatch elements***************************"
+awk -F "(" '{print $1}' ${TMP_DIR_BASE_NAME}_unmatch | awk '{print $NF}' | sort |uniq
+echo ""
